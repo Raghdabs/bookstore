46f393b6f4bdbfc8b059aa85fb893e988870b603
Malek <malek.benyedder792@gmail.Com>
2020-11-09 15:55:14 +0100

    Step_2

---
+++ src/bookstore/entities/Book.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookstore.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Loukas
+ */
+public class Book {
+    
+    private int id;
+    private String title;
+    private String Author;
+    private double Price;
+    private Date ReleaseDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String Author) {
+        this.Author = Author;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double Price) {
+        this.Price = Price;
+    }
+
+    public Date getReleaseDate() {
+        return ReleaseDate;
+    }
+
+    public void setReleaseDate(Date ReleaseDate) {
+        this.ReleaseDate = ReleaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "id=" + id + ", title=" + title + ", Author=" + Author + ", Price=" + Price + ", ReleaseDate=" + ReleaseDate + '}';
+    }
+    
+    public void insertBook(String title,String author,double price,Date relaseDate) {
+        
+    }
+}
