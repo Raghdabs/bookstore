e53b65e6d035d455d99e5fe10bebaceaef49db3e
Malek <malek.benyedder792@gmail.Com>
2020-11-16 17:07:52 +0100

    Update the update methode

--- src/bookstore/gui/UpdateForm.java
+++ src/bookstore/gui/UpdateForm.java
@@ -17,7 +17,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
