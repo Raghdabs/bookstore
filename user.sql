749ea1c8af363616bed095042a23e934a26f8f89
Raghdabs <bensassiraghda@gmail.com>
2020-11-17 11:35:03 +0100

    app javaaaa

--- user.sql
+++ user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 16 nov. 2020 à 11:34
--- Version du serveur :  10.4.13-MariaDB
--- Version de PHP : 7.4.7
+-- Généré le : mar. 17 nov. 2020 à 11:33
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.3.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,16 +34,19 @@ CREATE TABLE `user` (
   `username` varchar(20) DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
   `adress` varchar(30) DEFAULT NULL,
-  `password` varchar(20) NOT NULL
+  `password` varchar(20) NOT NULL,
+  `UserType` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `firstname`, `lastname`, `username`, `birthdate`, `adress`, `password`) VALUES
-(1, 'Malek', 'Benyedder', 'Loukas', '2020-11-13', 'BenArous,Rades', 'loukas123'),
-(2, 'Tony', 'Tonita', 'loukatonie', '2020-11-11', 'Tunis', '123');
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `username`, `birthdate`, `adress`, `password`, `UserType`) VALUES
+(1, 'Malek', 'Benyedder', 'Loukas', '2020-11-13', 'BenArous,Rades', 'loukas123', 'Admin'),
+(2, 'Tony', 'Tonita', 'loukatonie', '2020-11-11', 'Tunis', '123', 'Admin'),
+(3, 'client', 'client', 'client', '2020-11-04', 'clientadresse', 'client', 'Client'),
+(4, 'admin', 'admin', 'admin', '2020-11-04', 'adminadresse', 'admin', 'Admin');
 
 --
 -- Index pour les tables déchargées
@@ -63,7 +66,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
