d5584c1bb0e256512132719b311e2983baddafdc
Malek <malek.benyedder792@gmail.Com>
2020-12-16 15:45:51 +0100

    integration d'une template

---
+++ Application Angular+Spring/bookstore_Front_End/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddBookComponent } from './book/add-book/add-book.component';
+import { ListBookComponent } from './book/list-book/list-book.component';
+import { UpdateBookComponent } from './book/update-book/update-book.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'basic-ui', loadChildren: () => import('./basic-ui/basic-ui.module').then(m => m.BasicUiModule) },
+  { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsDemoModule) },
+  { path: 'forms', loadChildren: () => import('./forms/form.module').then(m => m.FormModule) },
+  { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
+  { path: 'icons', loadChildren: () => import('./icons/icons.module').then(m => m.IconsModule) },
+  { path: 'icons', loadChildren: () => import('./icons/icons.module').then(m => m.IconsModule) },
+  { path: 'general-pages', loadChildren: () => import('./general-pages/general-pages.module').then(m => m.GeneralPagesModule) },
+  { path: 'apps', loadChildren: () => import('./apps/apps.module').then(m => m.AppsModule) },
+  { path: 'user-pages', loadChildren: () => import('./user-pages/user-pages.module').then(m => m.UserPagesModule) },
+  { path: 'error-pages', loadChildren: () => import('./error-pages/error-pages.module').then(m => m.ErrorPagesModule) },
+  { path: "listBook", component: ListBookComponent },
+  { path: "addBook", component: AddBookComponent },
+  { path: "updateBook/:id", component: UpdateBookComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Application Angular+Spring/bookstore_Front_End/src/app/book/list-book/list-book.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../BookService/book.service';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-list-book',
+  templateUrl: './list-book.component.html',
+  styleUrls: ['./list-book.component.css']
+})
+export class ListBookComponent implements OnInit {
+
+  books: any;
+  constructor(private service: BookService, private router: Router) { }
+  ngOnInit() {
+  this.service.listBooks().subscribe(
+  response => {
+  this.books = response;
+  }
+  );
+  }
+ 
+  deleteBook(myObj) {
+   //console.log(this.provider);
+   this.service.deleteBook(myObj).subscribe(response => {
+   console.log(response);
+   this.refreshListBooks();
+   })
+   }
+   refreshListBooks() {
+   this.service.listBooks().subscribe(
+   response => {
+   this.books = response;
+   }
+   );
+   }
+   updateBook(myObj) {
+   this.router.navigate(['updateBook' + '/' + myObj['id']]);
+   }
+   addBook() {
+    this.router.navigate(['addBook']);
+    }
+}
